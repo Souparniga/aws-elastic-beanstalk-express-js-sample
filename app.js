6832ee6977b7ba8e32f7d721bba7071c001eb175
Souparniga <159101369+Souparniga@users.noreply.github.com>
2024-04-04 10:10:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello deva2222!'));
+app.get('/', (req, res) => res.send('Hello Souparniga!!  Are u excited about devops??));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
